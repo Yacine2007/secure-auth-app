8eff84c65570a21f54dbe1bd323334c4d82f81aa
B.Y PRO <152791424+Yacine2007@users.noreply.github.com>
2025-08-14 16:33:01 +0100

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
 const authController = require('../controllers/authController');
 const { protect } = require('../middlewares/authMiddleware');
-const upload = require('./multerConfig');
+const upload = require('./multerConfig'); // تأكد أن الملف في نفس المجلد
 
+// تسجيل مستخدم جديد مع رفع صورة
 router.post('/signup', upload.single('profileImage'), authController.signup);
+
+// التحقق من الكود
 router.post('/verify', authController.verifyCode);
+
+// تسجيل الدخول
 router.post('/login', authController.login);
+
+// استعادة كلمة المرور
 router.post('/forgot-password', authController.forgotPassword);
+
+// إعادة تعيين كلمة المرور
 router.post('/reset-password', authController.resetPassword);
 
+// تصدير الراوت
 module.exports = router;
