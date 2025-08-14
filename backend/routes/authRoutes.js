1213d68530368002a15c687edcde0eef71f1ae18
B.Y PRO <152791424+Yacine2007@users.noreply.github.com>
2025-08-14 14:20:25 +0100

    Update and rename auth.js to authRoutes.js

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
+const { protect } = require('../middlewares/authMiddleware');
+const upload = require('../config/multer');
+
+router.post('/signup', upload.single('profileImage'), authController.signup);
+router.post('/verify', authController.verifyCode);
+router.post('/login', authController.login);
+router.post('/forgot-password', authController.forgotPassword);
+router.post('/reset-password', authController.resetPassword);
+
+module.exports = router;
